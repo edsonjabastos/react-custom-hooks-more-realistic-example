50cdeb1b03e4a0edbf864e2eb7a9ac4e9e41ad41
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-13 21:43:09 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,25 +6,20 @@ import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  };
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    { url: process.env.REACT_APP_FIREBASE_CONNECTION },
-    transformTasks
-  );
-
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      { url: process.env.REACT_APP_FIREBASE_CONNECTION },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
